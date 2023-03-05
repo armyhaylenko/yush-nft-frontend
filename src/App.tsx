add1ebb5ff844f523c8089348e31fe8e3138379b
Alina Shcherbatiuk <alinashcherbatiuk@Alinas-MacBook-Pro.local>
2023-03-05 20:29:14 +0200

    Created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Header from "./components/Header/Header";
+import Header1 from "./components/Header/Header1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '100vh',
+        background: 'linear-gradient(154.76deg, #BBD4CE 12.74%, rgba(253, 235, 211, 0.71) 51.72%)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    },
+}));
+export default function App() {
+const classes = useStyles();
+return (
+    <div className={classes.root}>
+        <Header/>
     </div>
-  );
+);
 }
-
-export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './styles.css'
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+export default function Header() {
+    const pages = ['Discover', 'About', 'Artists',  'Help'];
+return (
+    <div className="header">
+        <div className="logo">
+            <div className="female"/>
+            <div className="name">
+                YUSH
+            </div>
+        </div>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex',
+                    justifyContent: 'center' }}}>
+                {pages.map((page) => (
+                    <Button
+                        key={page}
+                        sx={{ my: 2,
+                            color: '#554D4D',
+                            display: 'block',
+                            fontFamily: 'Poppins, sans-serif',
+                            fontWeight: 400,
+                            fontSize: '16px',
+                            lineHeight: '24px'
+                        }}
+                    >
+                        {page}
+                    </Button>
+                ))}
+            </Box>
+            <Button variant="outlined" sx={{ my: 2,
+                color: '#FC6327',
+                display: 'block',
+                fontFamily: 'Nunito, sans-serif',
+                fontWeight: 700,
+                fontSize: '16px',
+                lineHeight: '22px',
+                borderRadius:'8px',
+                border: '1px solid #FC6327',
+                marginRight:'167px',
+
+            }}>Connect Wallet</Button>
+    </div>
+);
+}
