5ef2f2f95ffcbf393ce1135007a10fd08d9fe786
nadia <66677695+se7enteenblue@users.noreply.github.com>
2023-04-10 21:28:37 +0300

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
 export default function Footer() {
-
-return (
-<footer>
-<div className='footer-container'>
-<div className="parent">
-<div className="div1"></div>
-<div className="div2"> 
-<h2 className='socials-title'>
-<a href="#"></a>
-</h2>
-</div>
-<div className="div3"> </div>
-</div>
-</div>
-</footer>
-);
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="parent">
+          <div className="div1"></div>
+          <div className="div2">
+            <h2 className="socials-title">Socials</h2>
+            <div className="socials">
+              <a href="#">Facebook</a>
+              <a href="#">Linkedin</a>
+              <a href="#">Instagram</a>
+              <a href="#">Twitter</a>
+            </div>
+          </div>
+          <div className="div3">
+          <h2 className="socials-title">News letter</h2>
+          
+          <label htmlFor="email">Enter your email address</label>
+          <input type="email" id="email"></input>
+          <div className="socials">
+          <a href="tel:+380931234567">+380 93 123 45 67</a>
+          <a href="mailto:abc@example.com">abc@example.com</a>
+          </div>
+          </div>
+        </div>
+        <p>© 2023 YUSH NFT. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
