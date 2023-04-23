a62b999fc94c13e0cefbacedd49843fd523700bb
Demon4ort <67164356+demon4ort@users.noreply.github.com>
2023-04-23 18:26:04 +0300

    minted counter connected

--- src/components/Counter/MyCounter.tsx
+++ src/components/Counter/MyCounter.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './styles.css'
+import {mintedCount} from "../../graph/GraphService";
 
 export default function MyCounter() {
+    const [count, setMinted] = useState<number | null>(null);
+
+    useEffect(() => {
+        const minted = async () => {
+            const result = await mintedCount();
+            setMinted(result);
+        };
+
+        minted();
+    }, []);
+
 
 return (
  <section className='section-counter'>
 
     <div className="counter-wrap">
             <div className="count">
-                <h2 className="num">20</h2>
+                <h2 className="num">{count}</h2>
                 <p className="num-title"> tokens minted</p>
             </div>
             <div className="count">
--- src/graph/GraphService.ts
+++ src/graph/GraphService.ts
@@ -35,19 +35,17 @@ export interface NFT {
     "collection_name": string
 }
 interface Data {
-    "data": {
-        "current_token_ownerships_aggregate": {
-            "aggregate": {
-                "count": number
-            }
-        },
-        "current_token_ownerships": NFT[]
-    }
+    "current_token_ownerships_aggregate": {
+        "aggregate": {
+            "count": number
+        }
+    },
+    "current_token_ownerships": NFT[]
 }
  export async function toBeSoldOf(collection: string = DEFAULT_COLLECTION,
                                  limit: number = 10,
                                  offset: number = 0): Promise<Data> {
-    const query = `query ToBeSoldOfCollection(
+     const query = `query ToBeSoldOfCollection(
     $_resource: String = "0x3c42361676ed7b681c3531ecf3b5221caf8e3db75dc04a5669afa54f98287309",
     $_main: String = "0x0bdd921fa9b94f0621cd5370b4f54f1f675cc31aeb6f6a69900ac54c0fae4982",
     $collection: String = "d1e534aee3f87d3521be0da10c3930be0c954175e783565601f1f3f1be6a50a0",
@@ -82,10 +80,53 @@ interface Data {
         property_version
     }
 }`
-    const variables = {
-        "collection": collection,
-        "limit": limit,
-        "offset": offset,
+     const variables = {
+         "collection": collection,
+         "limit": limit,
+         "offset": offset,
+     }
+     return await executeQuery(query, variables) as Data;
+ }
+
+ export async function mintedCount(): Promise<number> {
+    const query = `query MintedCount($_resource: String = "0x3c42361676ed7b681c3531ecf3b5221caf8e3db75dc04a5669afa54f98287309") {
+  token_activities_aggregate(where: {creator_address: {_eq: $_resource}, transfer_type: {_eq: "0x3::token::MintTokenEvent"}}) {
+    aggregate {
+      count
     }
-    return await executeQuery(query, variables) as Data;
-}
+  }
+}`
+     const data = await executeQuery(query);
+     return data.token_activities_aggregate.aggregate.count;
+ }
+
+ interface Collection {
+     "collection_data_id_hash": string,
+     "collection_name": string,
+     "description": string
+ }
+
+ export async function collections(limit: number = 10,
+                                   offset: number = 0): Promise<Collection[]> {
+    const query = `query Collections($_resource: String = "0x3c42361676ed7b681c3531ecf3b5221caf8e3db75dc04a5669afa54f98287309", $limit: Int = 10, $offset: Int = 0) {
+  current_collection_datas(
+    where: {creator_address: {_eq: $_resource}}
+    order_by: {last_transaction_timestamp: desc}
+    limit: $limit
+    offset: $offset
+  ) {
+    collection_data_id_hash
+    collection_name
+    description
+  }
+}`
+     const variables = {
+         "limit": limit,
+         "offset": offset,
+     }
+     return await executeQuery(query, variables) as Collection[];
+ }
+
+
+
+
